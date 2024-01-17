3aa3abb7f7370852cad3cf649e5818138ad3c07d
SolaRita <ritasolacanals@gmail.com>
2024-01-17 12:23:09 +0100

    add accordion to card job

--- app/ui/card/card-job.tsx
+++ app/ui/card/card-job.tsx
@@ -9,21 +9,26 @@ interface Job {
 export default function CardJob(job: Job) {
   const { name, charge, period, description } = job;
   return (
-    <div className="block max-w p-6 bg-background-200 border border-background-300 rounded-lg shadow hover:bg-background-300">
-      <h1 className="md:text-lg text-2xl text-primary">{name}</h1>
-      <div className="flex flex-col md:justify-between pb-10">
+    <details
+      name="description"
+      className="block max-w p-6 bg-background-200 border border-background-300 rounded-lg shadow hover:bg-background-300"
+    >
+      <summary className="flex md:justify-between">
+        <h1 className="md:text-lg text-2xl text-primary">{name}</h1>
         <h2 className="md:text-sm text-lg font-semibold">{charge}</h2>
+      </summary>
+      <div className="flex flex-col md:justify-between pb-4">
         <p className="md:text-sm text-lg">{period}</p>
       </div>
-      <ul>
+      <ul className="ml-4">
         {description.map((description, i) => {
           return (
-            <li className="md:text-sm text-lg" key={i}>
+            <li className="md:text-sm text-lg list-disc" key={i}>
               {description}
             </li>
           );
         })}
       </ul>
-    </div>
+    </details>
   );
 }
