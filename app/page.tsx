ad6dfff6385c8c24caabb8c66d1a724d50e59457
SolaRita <ritasolacanals@gmail.com>
2024-01-20 16:24:38 +0100

    add icons in about mi section

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import CardIcons from "./ui/card/card-icons";
 import SectionHeader from "./ui/section/section-header";
 
 const section = {
@@ -9,10 +10,171 @@ const section = {
     "Estoy entusiasmada por contribuir con mi creatividad y habilidades técnicas en entornos que valoren tanto la excelencia técnica como la experiencia del usuario. Mi objetivo es seguir creciendo y aportando en el fascinante mundo del desarrollo FrontEnd.",
   ],
 };
+
+const stackIcons = [
+  {
+    title: "Lenguajes de programación",
+    icons: [
+      {
+        name: "JavaScript",
+        icon: "javascript",
+      },
+      {
+        name: "TypeScript",
+        icon: "typeScript",
+      },
+      {
+        name: "HTML5",
+        icon: "html",
+      },
+      {
+        name: "CSS3",
+        icon: "css",
+      },
+      {
+        name: "Sass",
+        icon: "sass",
+      },
+    ],
+  },
+  {
+    title: "Frameworks y librerías de JS",
+    icons: [
+      {
+        name: "Angular.JS",
+        icon: "angular",
+      },
+      {
+        name: "Next.JS",
+        icon: "next",
+      },
+      {
+        name: "React.JS",
+        icon: "react",
+      },
+    ],
+  },
+  {
+    title: "Frameworks UI",
+    icons: [
+      {
+        name: "Mantine UI",
+        icon: "mantine",
+      },
+      {
+        name: "Angular Material",
+        icon: "angularmaterial",
+      },
+      {
+        name: "Tailwind",
+        icon: "tailwind",
+      },
+    ],
+  },
+  {
+    title: "Diseño",
+    icons: [
+      {
+        name: "Firma",
+        icon: "figma",
+      },
+      {
+        name: "Photoshop",
+        icon: "photoshop",
+      },
+    ],
+  },
+  {
+    title: "Metodologías Agile",
+    icons: [
+      {
+        name: "Jira",
+        icon: "jira",
+      },
+      {
+        name: "Trello",
+        icon: "trello",
+      },
+      {
+        name: "Notion",
+        icon: "notion",
+      },
+      {
+        name: "Miró",
+        icon: "miro",
+      },
+    ],
+  },
+  {
+    title: "IDE",
+    icons: [
+      {
+        name: "Visual Studio Code",
+        icon: "vscode",
+      },
+      {
+        name: "Web Storm from Jet Brains",
+        icon: "webstorm",
+      },
+    ],
+  },
+
+  {
+    title: "Control de versiones",
+    icons: [
+      { name: "GitHub", icon: "github" },
+      {
+        name: "GitLab",
+        icon: "gitlab",
+      },
+    ],
+  },
+  {
+    title: "Sistemas operativos",
+    icons: [
+      {
+        name: "Ubuntu",
+        icon: "ubuntu",
+      },
+      {
+        name: "Windows",
+        icon: "windows",
+      },
+      {
+        name: "Apple",
+        icon: "apple",
+      },
+    ],
+  },
+  {
+    title: "Otros",
+    icons: [
+      {
+        name: "Postman",
+        icon: "postman",
+      },
+      {
+        name: "npm",
+        icon: "npm",
+      },
+      {
+        name: "Docker",
+        icon: "docker",
+      },
+    ],
+  },
+];
+
 export default function Home() {
+  console.log("stackIcons", stackIcons);
   return (
     <main>
       <SectionHeader {...section} />
+      <div className="flex gap-5 flex-wrap">
+        {stackIcons.map((stack: any, index: number) => (
+          <CardIcons key={index} title={stack.title} icons={stack.icons} />
+        ))}
+      </div>
     </main>
   );
 }
---
+++ app/ui/card/card-icons.tsx
@@ -0,0 +1,29 @@
+import Image from "next/image";
+
+export default function CardIcons({ title, icons }: any) {
+  //   console.log(stack);
+  //   const [title, icons] = stack;
+  return (
+    <article className="max-w grow p-5  bg-background-200 border border-background-300 rounded-lg shadow">
+      <h3 className="justify-center md:text-lg text-2xl text-primary font-semibold pb-6">
+        {title}
+      </h3>
+      <div>
+        <ul className="flex justify-center gap-9">
+          {icons.map((icon: any, index: number) => (
+            <li key={index}>
+              <Image
+                src={`icons/${icon.icon}.svg`}
+                alt={icon.name}
+                width={30}
+                height={30}
+                className="grayscale-filter"
+                title={icon.name}
+              ></Image>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </article>
+  );
+}
