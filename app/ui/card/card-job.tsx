84ce37b7b72e147ec1a811b31f94ef8d10610e98
Rita Solà <ritasolacanals@gmail.com>
2024-03-10 19:36:53 +0100

    add animation in card job

--- app/ui/card/card-job.tsx
+++ app/ui/card/card-job.tsx
@@ -13,36 +13,66 @@ interface Job {
 
 export default function CardJob(job: Job) {
   const { name, charge, period, description, href } = job;
-  const [isOpen, setIsOpen] = useState(true);
+  const [accordionOpen, setAccordionOpen] = useState(false);
 
-  const handleToggle = () => {
-    setIsOpen(!isOpen);
+  const handleAccordion = () => {
+    setAccordionOpen(!accordionOpen);
   };
   const isLinkAvailable = href && href.length > 0;
 
   return (
-    <article>
-      <details
-        open={isOpen}
-        name="description"
-        className="block max-w p-6 bg-background-200 border border-background-300 rounded-lg shadow hover:bg-background-300"
+    <article
+      className={`block max-w p-6 bg-background-200 border border-background-300 rounded-lg shadow transition-all duration-300 hover:bg-background-300 ${
+        accordionOpen ? "bg-background-300" : "bg-background-200"
+      }`}
+    >
+      <button
+        onClick={handleAccordion}
+        className="flex flex-col cursor-pointer w-full"
       >
-        <summary
-          onClick={handleToggle}
-          className="flex flex-col cursor-pointer"
-        >
+        <div className="flex justify-between pb-4 w-full">
           <h1 className="md:text-lg text-2xl text-primary font-semibold">
             {name}
           </h1>
-          <div className="flex flex-col md:flex-row md:justify-between pb-4">
-            <h2 className="md:text-sm text-lg text-body font-semibold mb-2 md:mb-0">
-              {charge}
-            </h2>
-            <p className="md:text-sm text-lg text-body">{period}</p>
-          </div>
-        </summary>
-
-        <ul className="ml-4">
+          <svg
+            className="fill-primary-soft shrink-0 ml-8"
+            width="16"
+            height="16"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <rect
+              y="7"
+              width="16"
+              height="2"
+              rx="1"
+              className={`transform origin-center transition duration-200 ease-out ${
+                accordionOpen ? "rotate-180" : ""
+              }`}
+            />
+            <rect
+              y="7"
+              width="16"
+              height="2"
+              rx="1"
+              className={`transform origin-center rotate-90 transition duration-200 ease-out ${
+                accordionOpen ? "rotate-180" : ""
+              }`}
+            />
+          </svg>
+        </div>
+        <div className="flex flex-col md:flex-row md:justify-between items-start pb-4 w-full">
+          <h2 className="md:text-sm text-lg text-body font-semibold mb-2 md:mb-0">
+            {charge}
+          </h2>
+          <p className="md:text-sm text-lg text-body">{period}</p>
+        </div>
+      </button>
+      <div
+        className={`overflow-hidden transition-max-height duration-300 ease-in-out ${
+          accordionOpen ? "max-h-96" : "max-h-0"
+        }`}
+      >
+        <ul className="ml-5">
           {description?.map((desc: string, i: number) => (
             <li className="md:text-sm text-lg list-disc mb-2 text-body" key={i}>
               {desc}
@@ -62,7 +92,7 @@ export default function CardJob(job: Job) {
             </Link>
           </div>
         )}
-      </details>
+      </div>
     </article>
   );
 }
